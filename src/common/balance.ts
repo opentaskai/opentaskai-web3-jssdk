1b51e1b29591cb597a110e8d2799579e1b11949d
tercel <tercel.yi@gmail.com>
2024-11-18 14:28:46 +0800

    remove seaport and export some funcitons

--- src/common/balance.ts
+++ src/common/balance.ts
@@ -3,7 +3,9 @@ import { BigNumber, Contract } from 'ethers';
 import { ERC1155ABI } from './abi/ERC1155';
 import { ERC20ABI } from './abi/ERC20';
 import { ERC721ABI } from './abi/ERC721';
-import type { ERC721, ERC20, ERC1155 } from '../libs/seaport1.1/typechain/index';
+import type { ERC721 } from './typechain/ERC721';
+import type { ERC20 } from './typechain/ERC20';
+import type { ERC1155 } from './typechain/ERC1155';
 import type { Item } from './types';
 import { isErc1155Item, isErc20Item, isErc721Item } from './item';
 
--- src/common/index.ts
+++ src/common/index.ts
@@ -8,3 +8,15 @@ import * as format from './format';
 export { format };
 
 export { TokenUtil } from './tokenUtil';
+
+import * as signatureHelper from './signature-helper';
+export { signatureHelper };
+
+import * as approval from './approval';
+export { approval };
+
+import * as balance from './balance';
+export { balance };
+
+import * as balanceAndApprovalCheck from './balanceAndApprovalCheck';
+export { balanceAndApprovalCheck };
